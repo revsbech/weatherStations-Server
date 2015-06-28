005daf470513a6cb93a32ae6cf0171a98f41ea80
Jan-Erik Revsbech <janerik@moc.net>
2015-06-28 16:42:46 +0200

    Testing new submit controller

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -4,22 +4,37 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiController {
 	/**
 	 * @Route("/api/submit")
-	 * @param array $data The data to index
 	 */
-	public function submitAction(array $data) {
-		$number = rand(0, 100);
+	public function submitAction(Request $request) {
+
+		$dataRaw = $request->request->get('data');
+		if ($dataRaw === NULL) {
+			exit("Error");
+		}
+		
 		$client = new \Elasticsearch\Client();
 		$params = array();
-		$params['body'] = array($data);
+		$params['body'] = json_decode($dataRaw);
 		$params['index'] = 'weather_station';
 		$params['type'] = 'weatherStationLog';
 		$ret = $client->index($params);
 		return new JsonResponse(array('status' => 'OK'));
+	}
+
+	/**
+	 * @param int $limit
+	 */
+	public function readAction($limit = 100) {
+		$client = new \Elasticsearch\Client();
+
+		$searchParams['body']['query']['match']['testField'] = 'abc';
 	}
 }
